0d854edd063df9e44c30ccd704f35c0c97071355
Polina Kolesnikova <polina@example.com>
2024-04-24 23:28:34 +0200

    adding roles and passwords

--- spring_security_hw/src/main/java/com/example/spring_security_hw/config/ProjectConfig.java
+++ spring_security_hw/src/main/java/com/example/spring_security_hw/config/ProjectConfig.java
@@ -1,45 +1,55 @@
 package com.example.spring_security_hw.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ProjectConfig{
 
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests((authorize) -> authorize
-//                .requestMatchers("/public-data").permitAll()
-//                .requestMatchers("/private-data").hasAnyRole("ADMIN")
-//                .anyRequest().authenticated()
-//        )
-//                .formLogin(login -> login
-//                        .defaultSuccessUrl("/")
-//                        .permitAll())
-//                .logout(logout -> logout
-//                        .logoutSuccessUrl("/"));
-//        return http.build();
-//
-//    }
-
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/public-data").permitAll()
                         .requestMatchers("/private-data").hasAnyRole("ADMIN")
                 )
                 .formLogin(login -> login
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/private-data")
+                        .defaultSuccessUrl("/")
                         .permitAll())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/public-data"));
         return http.build();
-
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 
 }
--- spring_security_hw/src/main/java/com/example/spring_security_hw/controller/WebController.java
+++ spring_security_hw/src/main/java/com/example/spring_security_hw/controller/WebController.java
@@ -2,6 +2,7 @@ package com.example.spring_security_hw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +17,5 @@ public class WebController {
     public String privateData(){
         return "private-data";
     }
+
 }
